0131fcf4fc940ea8ddcf606046c79c1db6a63c58
Krzysztof Krawczyk <krawczykk@hotmail.com>
2016-06-14 11:03:24 +0200

    Poprawka poprzedniej poprawki, przy okazji kolory niebieskich kart

--- Core/CHandCard.cpp
+++ Core/CHandCard.cpp
@@ -18,7 +18,7 @@ CHandCard::CHandCard(CCard* origin, QWidget * parent) : CExtendedSignalWidget(pa
     QLabel* item = new QLabel();
     setText(QString("<h2>%1</h2>").arg(origin->getCityName()));
     setObjectName(QString("CardInHand_%1").arg(origin->getCityName()));
-    QString fontColor = type == BLACK ? "white" : "black";
+    QString fontColor = type == BLACK || type==BLUE ? "white" : "black";
     QString backgroundColor = type == UNKNOWN ? "orange; font-style: italic" : DiseaseType_SL[type];
     setStyleSheet(QString("border: 2px outset grey; border-radius: 10px; background-color: %1; color: %2;").arg(backgroundColor).arg(fontColor));
     setToolTip(QString("<img src=\"%1\" width=\"303\"/>").arg(origin->getSrc()));
